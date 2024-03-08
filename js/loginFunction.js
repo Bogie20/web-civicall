21bf08fb6d8617826344919d9c589331b82febbc
kbarpilleda <BCArpilleda@gmail.com>
2024-03-08 10:50:46 +0800

    goods

--- js/loginFunction.js
+++ js/loginFunction.js
@@ -12,7 +12,18 @@ import {
   signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
 
-import firebaseConfig from "/js/firebaseConfig.js";
+  const firebaseConfig = {
+  apiKey: "AIzaSyAMl8Eq_Clwyaw4w4D_TTrmOan9osHooMY",
+  authDomain: "civicall-app-a54d1.firebaseapp.com",
+  databaseURL:
+    "https://civicall-app-a54d1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "civicall-app-a54d1",
+  storageBucket: "civicall-app-a54d1.appspot.com",
+  messagingSenderId: "909514632764",
+  appId: "1:909514632764:web:bc969936e31a4fba9e9d39",
+  measurementId: "G-0BVYE5XJGV",
+};
+
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
